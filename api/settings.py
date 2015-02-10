00d800bdd2e964417449d03ecdd190b9bd9bc632
Marcus Gomes <mvp.gomes1989@gmail.com>
2015-02-10 22:56:25 +0000

    Fix some issues.

--- api/settings.py
+++ api/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello'
+    'pools'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -51,9 +51,9 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'gettingstarted.urls'
+ROOT_URLCONF = 'api.urls'
 
-WSGI_APPLICATION = 'gettingstarted.wsgi.application'
+WSGI_APPLICATION = 'api.wsgi.application'
 
 
 # Database
--- api/urls.py
+++ api/urls.py
@@ -3,15 +3,15 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+import pools.views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'gettingstarted.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    url(r'^$', pools.views.index, name='index'),
+    url(r'^db', pools.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
 
 )
