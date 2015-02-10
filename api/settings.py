aad51bfb5c9c9fe3b8533f987a2ce5610ca8a0dc
Marcus Gomes <mvp.gomes1989@gmail.com>
2015-02-10 22:22:18 +0000

    Rename folders and setup mysql database.

--- api/settings.py
+++ api/settings.py
@@ -61,8 +61,12 @@ WSGI_APPLICATION = 'gettingstarted.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.path.join(BASE_DIR, 'db.mysql'),
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
