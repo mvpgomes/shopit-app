e97dee6ec7c49cf3d33803504c7269a41c4d0a0f
Marcus Gomes <mvp.gomes1989@gmail.com>
2015-02-14 21:42:48 +0000

    Add the view serializer for the account model.

--- authentication_app/views.py
+++ authentication_app/views.py
@@ -1,19 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import permissions, viewsets
 
-from .models import Greeting
+from authentication_app.models import Account
+from authentication_app.permissions import IsAccountOwner
+from authentication_app.serializers import AccountSerializer
 
-# Create your views here.
-def index(request):
-    return HttpResponse('Hello from Python!')
+'''
+    @name : AccountViewSerializer
+    @desc : Defines the serializer for the account view.
+'''
+class AccountViewSerializer(viewsets.ModelViewSet):
+    lookup_field = 'username'
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
 
+    def get_permissions(self):
+        if self.reqiest.method in permissions.SAFE_METHODS:
+            return (permissions.AllowAny(),)
+        if self.request.method == 'POST':
+            return (permissions.AllowAny(),)
 
-def db(request):
+        return (permissions.IsAuthenticated(), IsAccountOwner(),)
 
-    greeting = Greeting()
-    greeting.save()
+    def create(self, request):
+        serializer = self.serializer_class(data=reqiest.data)
 
-    greetings = Greeting.objects.all()
+        if serializer.is_valid():
+            Account.objects.create_user(**serializer.validated_data)
+            return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
 
-    return render(request, 'db.html', {'greetings': greetings})
-
+        return Response({
+            'status' : 'Bad Request',
+            'message' : 'Account could not be created with the received data.'
+        }, status=status.HTTP_400_BAD_REQUEST)
