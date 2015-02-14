fa9577f875c999ea876c99e30614051f7ceba129
Marcus Gomes <mvp.gomes1989@gmail.com>
2015-02-14 20:10:51 +0000

    Add the account manager that support the creation of accounts.

--- authentication_app/models.py
+++ authentication_app/models.py
@@ -2,6 +2,32 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
 '''
+    @name : AccountManager
+    @desc : AccountManager model. The AccountManager is responsible to manage
+    the creation of users and superusers.
+'''
+class AccountManager(BaseUserManager):
+    def create_user(self, email, password=None, **kwargs):
+        if not email:
+            raise ValueError('Users must have a valid email adress.')
+        if not kwargs.get('username'):
+            raise ValueError('Users must have a valid username.')
+
+        account = self.model(
+            email = self.normalize_email(email), username= kwargs.get('username')
+        )
+
+        account.set_password(password)
+        account.save()
+        return account
+
+    def create_superuser(self, email, password, **kwargs):
+        account = self.create_user(email, password, **kwargs)
+        account.is_admin = True
+        account.save()
+        return account
+
+'''
     @name : Account
     @desc : Account model. This model is generic to represent a user that has an
     account in the ShopIT application. This user can be the store manager or the
