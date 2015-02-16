2de3b84f4abb3e3d130e6019c44e5b1601a5f638
Marcus Gomes <mvp.gomes1989@gmail.com>
2015-02-16 20:46:56 +0000

    Fix the static files path.

--- shopit_app/settings.py
+++ shopit_app/settings.py
@@ -88,11 +88,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATIC_ROOT = os.path.join(os.path.abspath(
+    os.path.join(BASE_DIR, 'shopit_app', 'static')), '')
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
