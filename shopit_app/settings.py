ed4440bd073543b53db2aa5b7bb5c34dae31b0c2
Marcus Gomes <mvp.gomes1989@gmail.com>
2015-02-16 11:43:10 +0000

    Update some config parameters to support static files on heroku.

--- shopit_app/settings.py
+++ shopit_app/settings.py
@@ -100,9 +100,10 @@ STATICFILES_FINDERS = (
     'compressor.finders.CompressorFinder',
 )
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Parse database configuration from $DATABASE_URL
-DATABASES['default'] =  dj_database_url.config()
+# DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
