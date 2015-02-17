b123001ea0d4fb475184727c39eafd5b46cc0964
Marcus Gomes <mvp.gomes1989@gmail.com>
2015-02-17 11:46:35 +0000

    Add the endpoint for the logout.

--- shopit_app/urls.py
+++ shopit_app/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import include, patterns, url
 from rest_framework_nested import routers
 
 from shopit_app.views import IndexView
-from authentication_app.views import AccountViewSet, LoginView
+from authentication_app.views import AccountViewSet, LoginView, LogoutView
 
 router = routers.SimpleRouter()
 router.register(r'accounts', AccountViewSet)
@@ -12,6 +12,7 @@ router.register(r'accounts', AccountViewSet)
 urlpatterns = patterns('',
     # API endpoints
     url(r'^api/v1/', include(router.urls)),
+    url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
     url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
     url('^.*$', IndexView.as_view(), name='index'),
 )
