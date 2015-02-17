4b6d30d8d313756461214b64d0cd0e7a039ba2a4
Marcus Gomes <mvp.gomes1989@gmail.com>
2015-02-17 12:17:14 +0000

    Add a controller for the navbar.

---
+++ static/javascripts/layout/navbar.controller.js
@@ -0,0 +1,31 @@
+/**
+* NavBarController
+* @namespace shopit_app.layout.controllers
+*/
+(function () {
+  'use strict';
+
+  angular
+  .module('shopit_app.layout.controllers')
+  .controller('NavbarController', NavbarController);
+
+  NavbarController.$inject = ['$scope', 'Authentication'];
+
+  /**
+  * @namespace NavbarController
+  */
+  function NavbarController($scope, Authentication) {
+    var nc = this;
+
+    nc.logout = logout;
+
+    /**
+    * @name logout
+    * @desc Log the user out
+    * @memberOf shopit_app.layout.controllers.NavbarController
+    */
+    function logout(){
+      Authentication.logout();
+    }
+  }
+})();
