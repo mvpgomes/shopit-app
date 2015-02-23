9168abf53be960d1630ec6ecb01c6d8f55d21739
Marcus Gomes <mvp.gomes1989@gmail.com>
2015-02-23 21:35:18 +0000

    Add the promotion manager to the promotions app.

--- promotions_app/models.py
+++ promotions_app/models.py
@@ -3,7 +3,27 @@ from django.db import models
 from authentication_app.models import Account
 
 '''
-    @name : Promotion
+    @name : PromotionManager.
+    @desc : The PromotionManager is responsible to create, delete and update the promotions.
+'''
+class PromotionManager(models.Manager):
+
+    def create_promotion(self, name, desc, expire_date, image):
+        promotion = self.create(name=name, desc=desc, expire_date=expire_date, image=image)
+        promotion.save()
+        return promotion
+
+    def delete_promotion(self, name):
+        promotion = super(PromotionManager, self).get_queryset().filter(name=name)
+        promotion.delete()
+
+    def update_promotion(self, name, expire_date):
+        promotion = super(PromotionManager, self).get_queryset().filter(name=name)
+        promotion.set_expire_date(expire_date)
+        return promotion
+
+'''
+    @name : Promotion.
     @desc : The promotion model.
 '''
 class Promotion(models.Model):
@@ -17,6 +37,8 @@ class Promotion(models.Model):
     expire_date = models.DateTimeField(auto_now=True)
 
     image = models.ImageField(upload_to='promotions')
+
+    objects = PromotionManager()
 
     def _unicode_(self):
         return self.name
