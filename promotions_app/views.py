3c73361dcc373059e3884b6f4a0c1a7f8024831c
Marcus Gomes <mvp.gomes1989@gmail.com>
2015-03-03 21:18:42 +0000

    Add travis.yaml

--- promotions_app/views.py
+++ promotions_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from rest_framework import permissions, status, views, viewsets
+from rest_framework.response import Response
+
+from promotions_app.models import Promotion
+from promotions_app.serializers import PromotionSerializer
+
+'''
+    @name : PromotionViewSet
+    @desc : Defines the promotion viewset.
+'''
+class PromotionViewSet(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = PromotionSerializer
+    pass
